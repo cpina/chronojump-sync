76bf2ed1c538f1eb2108a8334594f57f3b4f5f9c
Carles Pina i Estany <carles@pina.cat>
2015-07-23 16:39:56 +0200

    Adds the serverPersonId column on each table

--- create_mysql_tables.py
+++ create_mysql_tables.py
@@ -22,18 +22,23 @@ def drop_chronojump_table(db_mysql, cur_mysql):
     cur_mysql.execute("DROP DATABASE chronojump")
     cur_mysql.execute("CREATE DATABASE chronojump")
 
+def add_person_id(sql):
+    sql = sql.replace(" (", "( serverPersonId INT, ")
+    return sql
+
 def main():
     (db_mysql, cur_mysql) = connect_mysql()
     (db_sqlite, cur_sqlite) = connect_sqlite()
 
     drop_chronojump_table(db_mysql, cur_mysql)
-
     (db_mysql, cur_mysql) = connect_mysql()
 
+    # Creates tables
     cur_sqlite.execute("SELECT sql FROM sqlite_master WHERE type='table'")
 
     for sql_table in cur_sqlite.fetchall():
-        sql_table = sql_table[0]
+        sql_table = add_person_id(sql_table[0])
+        print sql_table
 
         cur_mysql.execute(sql_table)
 
